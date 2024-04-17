0f2fc498be101a3d3b151bc3eaf82f9207ac6c1b
Helena Utzig <hutzig@c1r5p4.hive.fi>
2024-04-17 19:28:26 +0300

    upload ft files

---
+++ ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 18:31:23 by hutzig            #+#    #+#             */
+/*   Updated: 2024/04/17 19:14:17 by hutzig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char	*s;
+
+	s = "  -  12344445";
+	printf("atoi = %d", atoi(s));
+	printf("\nft_atoi = %d", ft_atoi(s));
+}
